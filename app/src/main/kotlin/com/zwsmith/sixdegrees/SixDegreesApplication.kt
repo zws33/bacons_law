f03b116909c0e3f831c1db08c93c7c031c20362b
Zach Smith <zachsmith@okcupid.com>
2022-08-29 18:05:15 -0500

    initial commit

---
+++ app/src/main/kotlin/com/zwsmith/sixdegrees/SixDegreesApplication.kt
@@ -0,0 +1,6 @@
+package com.zwsmith.sixdegrees
+
+import android.app.Application
+
+class SixDegreesApplication: Application() {
+}
---
+++ app/src/main/kotlin/com/zwsmith/sixdegrees/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.zwsmith.sixdegrees.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+
+
+
